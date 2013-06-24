45420db1bf0161ca7289124b388c46e2575fade9
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-06-24 17:49:17 +0000

    Use setuptools instead of distutils (since we can include install_requires)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 description = 'Functions and data manipulation for economics data'
 with open('README.rst') as readme:
